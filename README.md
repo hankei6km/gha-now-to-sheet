8e5a58b49362cabc39aed7019395cd6304a41665
hankei6km <gh-hankei6km@outlook.jp>
2022-03-11 15:59:09 +0900

    Implement

---
+++ README.md
@@ -0,0 +1,9 @@
+# gha-now-to-sheet
+
+GitHub Actions から `Date.now()` の値を Google Spread Sheet に書き込む。
+
+## License
+
+MIT License
+
+Copyright (c) 2022 hankei6km
---
+++ src/index.ts
@@ -0,0 +1,65 @@
+#!/usr/bin/env zx
+import 'zx/globals'
+import { sheets as sheetsAPI } from '@googleapis/sheets'
+import { GoogleAuth } from 'google-auth-library'
+import { JSONClient } from 'google-auth-library/build/src/auth/googleauth'
+
+async function main(
+  auth: GoogleAuth<JSONClient>,
+  runId: number,
+  step: number,
+  spreadsheetId: string,
+  sheetName: string
+) {
+  const authClient = await auth.getClient()
+  const sheets = sheetsAPI({ version: 'v4', auth: authClient })
+  try {
+    const now = Date.now()
+    const request = {
+      spreadsheetId,
+      range: `${sheetName}!A2:C`,
+      valueInputOption: 'USER_ENTERED',
+      insertDataOption: 'INSERT_ROWS',
+      resource: {
+        majorDimension: 'ROWS',
+        values: [[runId, step, now]]
+      },
+      auth: authClient
+    }
+    const addRes = await sheets.spreadsheets.values.append(request)
+  } catch (err) {
+    console.log('The API returned an error: ' + err)
+  }
+}
+
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+const auth = new GoogleAuth({
+  scopes: SCOPES
+})
+
+const spreadsheetId = process.env['SPREADSHEET_ID']
+const sheetName = process.env['SHEET_NAME']
+let runId = -1
+let step = -1
+if (typeof argv['run-id'] === 'number') {
+  runId = argv['run-id']
+}
+if (typeof argv['step'] === 'number') {
+  step = argv['step']
+}
+
+if (!spreadsheetId) {
+  console.error(chalk.red(`$SPREADSHEET_ID is not specified`))
+  process.exit(1)
+}
+if (!sheetName) {
+  console.error(chalk.red(`$SHEET_NAME is not specified`))
+  process.exit(1)
+}
+if (runId === -1 || step === -1) {
+  console.log('USAGE: script --run-in [RUN ID] --step [STEP]')
+
+  process.exit(1)
+}
+
+main(auth, runId, step, spreadsheetId, sheetName)
